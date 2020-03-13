352d241846059c2f6828a8c39dd6a23668604543
BLipsett <53913632+BLipsett@users.noreply.github.com>
2020-03-13 14:15:33 -0400

    express static

--- 11-express/htmlroutes.js
+++ 11-express/htmlroutes.js
@@ -1,15 +1,21 @@
 let path = require("path")
 
-module.exports = function (app) {
+module.exports = function(app) {
+
+
+  
 
 
 
-    app.get("/index", function (req, res) {
-        res.sendfile(path.join(__dirname, "../11-express/02-homework/Develop/public/index.html"))
+    app.get("*", function (req, res) {
+        res.sendfile(path.join(__dirname, "../11-express/02-Homework/Develop/public/index.html"))
     })
 
     app.get("/notes", function (req, res) {
-        res.sendfile(path.join(__dirname, "../11-express/02-homework/Develop/public/notes.html"))
+        res.sendfile(path.join(__dirname, "../11-express/02-Homework/Develop/public/notes.html"))
     })
 
+
+
 }
+//app.use('/static', express.static(path.join(__dirname, 'public')))
--- 11-express/server.js
+++ 11-express/server.js
@@ -1,11 +1,17 @@
-let express = require("express");
+const express = require("express");
 
-let app = express();
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//let PORT = 8080;
+
+
+let public = {};
+app.use(express.static("public", public));
+
+
+
 let PORT = process.env.PORT || 8088;
 
 app.listen(PORT, function () {
@@ -13,11 +19,6 @@ app.listen(PORT, function () {
     console.log(`Hello word ha not world at ${PORT}`);
 })
 
-console.log("listening on " + PORT)
-
-
-app.get('/index.html', function (req, res) {
-    res.send('Hello, World!')
-})
+require("./htmlRoutes")(app);
 
 
